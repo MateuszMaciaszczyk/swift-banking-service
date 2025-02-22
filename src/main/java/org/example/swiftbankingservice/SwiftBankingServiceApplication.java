e85e050d4e587a2ad86c611879e0df0705ed3028
MateuszMaciaszczyk <maciaszczyk@student.agh.edu.pl>
2025-02-22 13:39:03 +0100

    Initial commit

---
+++ src/main/java/org/example/swiftbankingservice/SwiftBankingServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.swiftbankingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftBankingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftBankingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/swiftbankingservice/SwiftBankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.swiftbankingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftBankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
