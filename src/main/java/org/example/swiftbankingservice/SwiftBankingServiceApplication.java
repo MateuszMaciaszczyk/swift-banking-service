b36c251d7d64bfdaa0f5cf1fe691d60e721dc3e7
MateuszMaciaszczyk <maciaszczyk@student.agh.edu.pl>
2025-02-23 11:54:49 +0100

    added endpoint 1

--- src/main/java/org/example/swiftbankingservice/SwiftBankingServiceApplication.java
+++ src/main/java/org/example/swiftbankingservice/SwiftBankingServiceApplication.java
@@ -1,13 +1,20 @@
 package org.example.swiftbankingservice;
 
+import org.example.swiftbankingservice.services.SwiftCodeCsvService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SwiftBankingServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SwiftBankingServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftBankingServiceApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner initDatabase(SwiftCodeCsvService swiftService) {
+        return args -> swiftService.loadSwiftCodesFromCsv("src/main/resources/swift-codes.csv");
+    }
 }
